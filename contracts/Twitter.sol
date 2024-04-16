10a32ae4b012662d0192ecf81780c2414449d04f
Guilherme Narguis <107137104+narguis@users.noreply.github.com>
2024-04-16 01:27:28 -0300

    create get self data functions

--- contracts/Twitter.sol
+++ contracts/Twitter.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.0;
 
 contract Twitter{
 
+    uint16 constant MAX_TWEET_LENGTH = 280;
+
     struct Tweet {
         address author;
         string content;
@@ -14,6 +16,9 @@ contract Twitter{
     mapping(address => Tweet[] ) public tweets;
 
     function createTweet(string memory _tweet) public {
+        
+        require(bytes(_tweet).length <= MAX_TWEET_LENGTH, "Character limit exceeded");
+        
         Tweet memory newTweet = Tweet({
             author: msg.sender,
             content: _tweet,
@@ -28,7 +33,15 @@ contract Twitter{
         return tweets[_owner][_i];
     }
 
+    function getMyTweet (uint _i) public view returns (Tweet memory) {
+        return tweets[msg.sender][_i];
+    }
+
     function getAllTweets (address _owner) public view returns (Tweet[] memory) {
         return tweets[_owner];
+    }
+
+    function getAllMyTweets () public view returns (Tweet[] memory){
+        return tweets[msg.sender];
     }
 } 
